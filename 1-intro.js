49766991a52883c75cafa26681dba8b453bcda63
Salvatore Dininni <s.dininni@yahoo.com>
2022-11-02 13:24:19 +0100

    modules implementations

---
+++ 1-intro.js
@@ -0,0 +1,17 @@
+//GLOBALS - NO WINDOWS!!!
+
+// globals = variables access them from anywhere in your application 
+
+// _dirname   - path to the current directory
+// _filename  - file name
+// require    - function to use models (CommonsJS)
+// module     - info about the current module (file)
+// process    - info about the env where the programm is been executed
+// console.log('testing the dirname', __filename);
+// console.log('testing the dirname', __dirname);
+setTimeout(() => {
+  console.log('====================================');
+  console.log("Hello World");
+  console.log('====================================');
+}, 1000)
+console.log("Hello Salvatore");
--- app.js
+++ app.js
@@ -1,9 +1,19 @@
-const amount = 15;
+// const amount = 15;
 
-if (amount < 10) {
-  console.log('small number');
-} else if (amount > 10 && amount < 13) {
-  console.log('large number');
-} else {
-  console.log('my first node app');
-}
+// if (amount < 10) {
+//   console.log('small number');
+// } else if (amount > 10 && amount < 13) {
+//   console.log('large number');
+// } else {
+//   console.log('my first node app');
+// }
+
+//CommonJs, every file is module (by default)
+//Modules - Encapsulated Code (only share a minum)
+//Every File in Node is a module
+
+const { john, peter } = require('./4-name');
+const sayHi = require('./5-utils')
+sayHi('susan');
+sayHi(peter);
+sayHi(john);
