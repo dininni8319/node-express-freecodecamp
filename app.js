f2f47e1b2325102312d080d18273d052046e9aaf
Salvatore Dininni <s.dininni@yahoo.com>
2022-11-02 11:28:51 +0100

    initial project commit

---
+++ app.js
@@ -0,0 +1,9 @@
+const amount = 15;
+
+if (amount < 10) {
+  console.log('small number');
+} else if (amount > 10 && amount < 13) {
+  console.log('large number');
+} else {
+  console.log('my first node app');
+}
